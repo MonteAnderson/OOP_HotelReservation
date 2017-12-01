29614c3e8431c4e6747d9ac6186bd159925f8e68
jackson-mediavilla <jame1409@colorado.edu>
2017-11-30 19:30:48 -0700

    Add Admin class skeleton

---
+++ HotelReservations/src/com/hotelres/Admin.java
@@ -0,0 +1,20 @@
+package com.hotelres;
+
+public class Admin {
+
+    public void createAccount(String userName, String password){
+
+    }
+
+    public void updateUser(String userName, String query){
+
+    }
+
+    public void updateDatabase(String query) {
+
+    }
+
+    public Hotel searchHotel(String location) {
+
+    }
+}
