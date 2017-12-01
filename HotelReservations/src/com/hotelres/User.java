80b4d083023ef255af049913c055ad868646000b
ncarmine <nathanacarmine@gmail.com>
2017-11-30 19:32:19 -0700

    Add User class skeleton

---
+++ HotelReservations/src/com/hotelres/User.java
@@ -0,0 +1,28 @@
+package com.hotelres;
+
+import javax.swing.text.html.ListView;
+import java.util.Map;
+
+public class User {
+    private Booking[] bookings;
+
+    public Bookings[] getBookings() {
+        return bookings;
+    }
+
+    public void bookHotel(Hotel hotel) {
+
+    }
+
+    public void addBooking(Booking booking) {
+
+    }
+
+    public void cancelBooking(Booking booking) {
+
+    }
+
+    public Hotel[] searchHotel(String location) {
+        return Hotel[];
+    }
+}
