973cccd6b7300cf07e6c3e35e454e9cbf1fa2dfb
Monte Anderson <MRAnd023@gmail.com>
2017-12-08 16:46:16 -0700

    Update HotelListIterator.java

--- HotelReservations/src/com/hotelres/HotelListIterator.java
+++ HotelReservations/src/com/hotelres/HotelListIterator.java
@@ -1,10 +1,12 @@
 package com.hotelres;
+import java.util.Iterator;
 
-public class HotelListIterator extends Iterator {
+public class HotelListIterator implements Iterator {
     public boolean hasNext(){
         return true;
     }
+
     public Hotel next(){
-        
+
     }
 }
