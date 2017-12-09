beabd2f38e40e07c220195a6c87356bae6e44288
jackson-mediavilla <jame1409@colorado.edu>
2017-12-08 17:38:34 -0700

    Implemented HotelListIterator

--- HotelReservations/src/com/hotelres/HotelListIterator.java
+++ HotelReservations/src/com/hotelres/HotelListIterator.java
@@ -1,12 +1,30 @@
 package com.hotelres;
 import java.util.Iterator;
+import java.util.List;
 
 public class HotelListIterator implements Iterator {
+
+    private List<Hotel> hotelList;
+    private int index;
+
+    public HotelListIterator(List<Hotel> hL){
+        this.hotelList = hL;
+        index = 0;
+    }
     public boolean hasNext(){
-        return true;
+        if ((index + 1) < hotelList.size()){
+            return true;
+        }
+        else
+            return false;
     }
 
     public Hotel next(){
-
+        if (this.hasNext()){
+            index++;
+            return hotelList.get(index);
+        }
+        else
+            return null;
     }
 }
