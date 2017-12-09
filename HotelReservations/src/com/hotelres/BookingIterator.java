e35369ff67303b682c2f9d3cea86e2114be9aa12
jackson-mediavilla <jame1409@colorado.edu>
2017-12-08 17:45:04 -0700

    Implemented BookingIterator

--- HotelReservations/src/com/hotelres/BookingIterator.java
+++ HotelReservations/src/com/hotelres/BookingIterator.java
@@ -1,7 +1,33 @@
-package com.hotelres
+package com.hotelres;
 
-public class BookingIterator extends Iterator {
-    public boolean hasNext(){}
-    public Booking next(){}
+import java.util.Iterator;
+import java.util.List;
+
+public class BookingIterator implements Iterator {
+
+    private List<Booking> bookingList;
+    private int index;
+
+    public BookingIterator(List<Booking> bL){
+        this.bookingList = bL;
+        index = 0;
+    }
+
+    public boolean hasNext(){
+        if ((index + 1) <  bookingList.size()) {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public Booking next(){
+        if (this.hasNext()){
+            index++;
+            return bookingList.get(index);
+        }
+        else
+            return null;
+    }
 
 }
