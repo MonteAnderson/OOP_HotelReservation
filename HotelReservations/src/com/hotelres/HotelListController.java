e6aec73e3bd605f97d37e0957dfc3283a50f03ae
Nolan Cretney <nokynokes@gmail.com>
2017-12-09 17:11:00 -0700

    swapped some code

--- HotelReservations/src/com/hotelres/HotelListController.java
+++ HotelReservations/src/com/hotelres/HotelListController.java
@@ -28,12 +28,13 @@ public class HotelListController {
 
     public List<Hotel> getHotels(Map<String, String> criteria) {
         if(validateCriteria(criteria)){
+
         }
     }
 
     public boolean validateCriteria(Map<String, String> criteria) {
         boolean ret = true;
-        for(String attr : criterias){
+        for(String attr : getCriterias()){
             if(!criteria.containsKey(attr)){
                 ret = false;
             }
